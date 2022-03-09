dc94017d791dd189d2eafc3bb5f56f0747dd0f4d
Elvis Onobo <elvis.onobo@gmail.com>
2022-03-09 13:52:51 +0100

    Chore: Setup RabbitMQ

---
+++ src/config/rabbitmq.ts
@@ -0,0 +1,50 @@
+import amqplib from 'amqplib'
+const rabbit = amqplib.connect('amqp://localhost')
+import eventsEmitter from '../events/events'
+
+export default class RabbitMQ {
+    /**
+     * Initialize RabbitMQ
+     */
+    private static async connect(){
+        return await (await rabbit).createChannel()
+    }
+
+    /**
+     * Publishes data to the queue
+     * @param channel 
+     * @param data 
+     */
+    public static async publish(channel:string, data:unknown): Promise<void>{
+        try {
+            const dataToPublish = JSON.stringify(data)
+            const process = await this.connect()
+            await process.assertQueue(channel)
+            process.sendToQueue(channel, Buffer.from(dataToPublish))
+        } catch (error) {
+            console.log('Error publishing data >>> ', error)
+        }
+    }
+
+    /**
+     * Consume published data from a channel
+     * @param channel 
+     */
+    public static async consume(channel:string, event:string): Promise<void> {
+        try {
+            const process = await this.connect()
+            process.assertQueue(channel)
+            process.consume(channel, (message)=> {
+                if(message !== null){
+                    const data = JSON.parse(message.content.toString())
+                    // emit an event to call the appropriate function
+                    const emitted = eventsEmitter.emit(event, data)
+                    console.log({emitted})
+                    process.ack(message)
+                }
+            })
+        } catch (error) {
+            console.log('Error consuming data >>> ', error)
+        }
+    }
+}
